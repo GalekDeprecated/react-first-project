a0c8ad034359387447c4b81f5d79a48d3946a2c1
Jan Galek <admin@gcore.cz>
2019-10-25 18:49:12 +0200

    Init

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Menu from "./Menu/Menu";
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <div className="row">
+                <Menu />
+            </div>
+        )
+    }
+}
+
+export default Header
